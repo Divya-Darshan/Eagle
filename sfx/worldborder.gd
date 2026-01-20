d97e23c7ac1873e17ce093b3004435794bd9e32b
Divya-Darshan <parukrish.darshan20@gmail.com>
2026-01-20 23:18:10 +0530

    about btn

--- sfx/worldborder.gd
+++ sfx/worldborder.gd
@@ -1,9 +1,11 @@
 extends Area2D
 
+
 @onready var val: AudioStreamPlayer2D = $val
 
 func _on_body_entered(body: Node2D) -> void:
-	val.play()       
+	val.play()
+	get_tree().create_timer(1.0).timeout       
 	get_tree().paused = true
 	var menu = get_tree().get_first_node_in_group("GameMenu")
 	if menu:
